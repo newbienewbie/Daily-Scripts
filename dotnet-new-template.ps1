4cb2b369171e55cf4a5efb15c66ecc72ba618aee
newbienewbie <itminus@hotmail.com>
2019-10-04 14:34:06 +0800

    Fix bug : multiple sdks lead to wrong package ver
    dotnet add package is always the  latest even there's a globaljson

--- dotnet-new-template.ps1
+++ dotnet-new-template.ps1
@@ -3,38 +3,49 @@ param(
 )
 
 
-# only pattern like 2.1.402  
-function Get-Latest-SDK-Version{
-
-    $sdks= dotnet --list-sdks |  
-    Where-Object{ 
-        $_ -Match "^(?<MajorVer>[\d]*)\.(?<MinorVer>[\d]*)\.(?<PatchVer>[\d]*)\s" 
-    } | 
-    ForEach-Object{
-        @{ 
+function Get-Version-Info ([string] $version)
+{
+    $r = $version -Match "^(?<MajorVer>[\d]*)\.(?<MinorVer>[\d]*)\.(?<PatchVer>[\d]*)" ;
+    if ($r) {
+        return @{ 
             MajorVer=$matches['MajorVer']; 
             MinorVer=$matches['MinorVer']; 
             PatchVer=$matches['PatchVer']; 
         }
     } 
+}
+
+function Get-Current-Version {
+    $v = dotnet --version;
+    Get-Version-Info $v
+}
+
+# only pattern like 2.1.402  
+function Get-Latest-SDK-Version{
+
+    $sdks= dotnet --list-sdks |  
+        Where-Object{ 
+            $_ -Match "^(?<MajorVer>[\d]*)\.(?<MinorVer>[\d]*)\.(?<PatchVer>[\d]*)\s" 
+        } | 
+        ForEach-Object{ Get-Version-Info $_ } ;
     return $sdks[-1]
 }
 
 
+$targetVersion = Get-Latest-SDK-Version
+$targetVersionString =  $targetVersion['MajorVer'] + "." + $targetVersion['MinorVer'] + "." + $targetVersion['PatchVer']
+# add a `global.json`
+dotnet new globaljson --sdk-version $targetVersionString
 
+$targetVersion = Get-Current-Version
 
-$latestSdkVersion = Get-Latest-SDK-Version
-$latestSdkVersionString =  $latestSdkVersion['MajorVer'] + "." + $latestSdkVersion['MinorVer'] + "." + $latestSdkVersion['PatchVer']
-# add `global.json`
-dotnet new globaljson --sdk-version $latestSdkVersionString
-
-
+## create the project
 dotnet new $arguments
 
-$wildVersion = $latestSdkVersion['MajorVer'] + "." + $latestSdkVersion['MinorVer'] + "." + '*'
-
 # add package for code generation
-dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design --version $wildVersion
-dotnet add package Microsoft.EntityFrameworkCore.Design --version $wildVersion # as for 2.2
+$wildVersion = $targetVersion['MajorVer'] + "." + $targetVersion['MinorVer'] + "." + '*'
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design --version $wildVersion --no-restore
+dotnet add package Microsoft.EntityFrameworkCore.Design --version $wildVersion --no-restore 
 
-# dotnet restore 
+## restore
+dotnet restore 
